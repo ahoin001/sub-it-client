6401ec29f6e1fe070df2c8c1c14118da0f783752
Alexander Hoinville <ahoin001@gmail.com>
2020-09-20 12:50:14 -0400

    Used Treact to change home page and adjust several settings

--- src/App.js
+++ src/App.js
@@ -1,35 +1,65 @@
 import React, { Component } from 'react'
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Login from './components/Login'
-import Register from './components/Register'
-// import Prueba from './components/Prueba'
-import ProjectsList from './components/ProjectsList'
-import Projectform from './components/Project_form';
-import Update from './components/Update';
-import ProjectDetails from './components/ProjectDetailPage';
+// *** Treact Things
+// import "style.css"
+import "tailwindcss/dist/base.css"
+
+import AnimationRevealPage from "./helpers/AnimationRevealPage"
+import Hero from "./treeponents/hero/BackgroundAsImage"
+import Features from './treeponents/features/TwoColWithSteps'
+import Summary from './treeponents/features/ThreeColWithSideImage'
+
+import Footer from './treeponents/footers/MiniCenteredFooter'
+// ****
+
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+// import Navbar from './components/Navbar'
+// import Home from './components/Home'
+// import Login from './components/Login'
+// import Register from './components/Register'
+
+// import ProjectsList from './components/ProjectsList'
+// import Projectform from './components/Project_form';
+// import Update from './components/Update';
+// import ProjectDetails from './components/ProjectDetailPage';
 
 class App extends Component {
   render() {
+    
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-        <Route exact path="/" component={Home} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Route path="/dashboard" component={ProjectsList} />
-            <Route path="/project" component={ProjectDetails} />
-            <Route path="/update" component={Update} />
-            <Route exact path="/form" component={Projectform} />
-          </div>
-        </div>
-      </Router>
+      <AnimationRevealPage disabled >
+        <Hero />
+        <Features/>
+        <Summary/>
+        <Footer/>
+      </AnimationRevealPage>
     )
+
+    // return (
+
+    //   <Router>
+    //     <div className="App">
+    //       <Navbar />
+
+    //       <Route exact path="/" component={Home} />
+
+    //       <div className="container">
+    //         <Route exact path="/register" component={Register} />
+    //         <Route exact path="/login" component={Login} />
+    //         <Route path="/dashboard" component={ProjectsList} />
+    //         <Route path="/project" component={ProjectDetails} />
+    //         <Route path="/update" component={Update} />
+    //         <Route exact path="/form" component={Projectform} />
+    //       </div>
+
+    //     </div>
+
+    //   </Router>
+
+    // )
+
   }
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Main from './Main';
 import Footer from './Footer';
 
 
-class Home extends Component {
-  render() {
-    return (
-         <div>
-           <Main />
-           <Footer/>
-          </div>
-    )
-  }
-}
+const Home = () => {
+  return (
+    <div>
+      <Main />
+      <Footer/>
+     </div>
+)
+};
 
-export default Home
-
-
-
-// import React from "react";
-
-
-// export default class Home extends React.Component {
-
-//     render(){
-//         return (
-//             <section>
-//                 <h1> Home Page </h1>
-//                 <p> Welcome to the best app ever!  </p>
-//             </section>
-//         )
-//     }
-// }
+export default Home;
