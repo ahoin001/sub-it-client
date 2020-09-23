18cc8ad0fd3a962a50683bc8b648334033f10a6f
Alexander Hoinville <ahoin001@gmail.com>
2020-09-22 23:18:31 -0400

    SignUp front end connected to backend and DB

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ import LoginTree from './pages/Login/Login.jsx'
 
 
 
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Login from './components/Login'
-import Register from './components/Register'
+// import Navbar from './components/Navbar'
+// import Home from './components/Home'
+// import Login from './components/Login'
+// import Register from './components/Register'
 
 import ProjectsList from './components/ProjectsList'
 import Projectform from './components/Project_form';
