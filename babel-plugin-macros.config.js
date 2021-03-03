84531d46269377755fde25f60a4a6368ea49e556
Alexander Hoinville <ahoin001@gmail.com>
2021-03-03 13:39:47 -0500

    Added tailwindconfig.js to babelmacros to resolve tailwind not recognizing default colors

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -1,7 +1,22 @@
-module.exports = {
-  twin: {
-    styled: "styled-components",
-    config: "./src/tailwind.config.js",
-    format: "auto"
-  }
-};
+// module.exports = {
+//   twin: {
+//     styled: "styled-components",
+//     config: "./src/tailwind.config.js",
+//     format: "auto"
+//   }
+// };
+
+// "babelMacros": {
+//   "twin": {
+//     "preset": "styled-components",
+//     "styled": {
+//       "import": "default",
+//       "from": "styled-components"
+//     },
+//     "config": "./src/tailwind.config.js",
+//     "css": {
+//       "import": "css",
+//       "from": "styled-components"
+//     }
+//   }
+// }
