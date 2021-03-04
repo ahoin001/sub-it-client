763bb62e0a59e2900e89e7d214a97acf26ef55bf
Alexander Hoinville <ahoin001@gmail.com>
2021-03-03 20:58:55 -0500

    Change file names to lowercase to fix deployment with heroku

---
+++ src/components/Subtitle/subtitle.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+// export const StationContainer = tw.div`flex`;
+
+export const FormInput = styled.input`
+     ${tw`w-full max-w-lg  py-4 rounded-lg
+          bg-gray-300 text-green-900 font-semibold
+  `}
+ 
+`;
